dbc422bde11b1b04217421356e02e61bed5f1577
kk6 <hiro.ashiya@gmail.com>
2021-05-12 20:30:14 +0900

    Chakra UI 導入

--- frontend/web-front/pages/index.tsx
+++ frontend/web-front/pages/index.tsx
@@ -1,38 +1,69 @@
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next"
 import Head from "next/head"
 import { getAllVideosData } from "../lib/videos"
-import styles from "../styles/Home.module.css"
+import {
+  Box,
+  Container,
+  Heading,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from "@chakra-ui/react"
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>
 
 const Video: React.FC = ({ video }) => {
   const url = `https://www.youtube.com/watch?v=${video.id}`
   return (
-    <p>
-      {video.title} / {video.scale} / {video.key} /{" "}
-      <a target="_blank" href={url} rel="noopener noreferrer">
-        link
-      </a>
-    </p>
+    <Tr>
+      <Td>{video.title}</Td>
+      <Td>{video.scale}</Td>
+      <Td>{video.key}</Td>
+      <Td>
+        <a target="_blank" href={url} rel="noopener noreferrer">
+          link
+        </a>
+      </Td>
+    </Tr>
   )
 }
 
 const Home: NextPage<Props> = (props) => {
   return (
-    <div className={styles.container}>
+    <Container maxW="container.xl">
       <Head>
         <title>Jam</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>Jam</h1>
-        <div>
-          {props.videos &&
-            props.videos.map((video) => <Video key={video.id} video={video} />)}
-        </div>
+        <Box padding="4">
+          <Heading>Jam</Heading>
+        </Box>
+
+        <Box padding="4">
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>タイトル</Th>
+                <Th>スケール</Th>
+                <Th>キー</Th>
+                <Th>YouTube</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {props.videos &&
+                props.videos.map((video) => (
+                  <Video key={video.id} video={video} />
+                ))}
+            </Tbody>
+          </Table>
+        </Box>
       </main>
-    </div>
+    </Container>
   )
 }
 
