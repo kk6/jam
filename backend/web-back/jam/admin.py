647fa971a4e053d018a08a48cc840c628deae67c
kk6 <hiro.ashiya@gmail.com>
2021-05-17 21:09:50 +0900

    インポートをCSVに限定

--- backend/web-back/jam/admin.py
+++ backend/web-back/jam/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportMixin
+from import_export.formats import base_formats
 from .models import Video
 
 
@@ -13,4 +14,7 @@ class VideoResource(resources.ModelResource):
 class VideoAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ("id", "title", "posted_at")
     list_display_links = ("title",)
+
+    # For django-import-export
     resource_class = VideoResource
+    formats = [base_formats.CSV]
